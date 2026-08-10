6ca1082dff2e8eedd0a6673554c97b7dab064a95
Barath2608 <sbbarathkumar@gmail.com>
2026-08-10 10:31:50 +0530

    Create pow4blk.v

---
+++ newitoh_233/pow4blk.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:54:43 03/13/2025 
+// Design Name: 
+// Module Name:    pow4blk 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pow4blk(
+    );
+
+
+endmodule
